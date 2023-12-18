41f552c476332b280f8552606fad85d84572b62c
Omar <omarsegura76300@gmail.com>
2023-12-18 19:34:31 +0100

    plusieurs modif

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,20 +1,25 @@
 const infos = document.getElementById('infos');
 
 
+
 fetch('assets/data/games.json')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-let id = 0
-        for (let index of data) {
+        let id = 0;
+
+       
+
+        for (const game of data) {
             const card = document.createElement('div');
             card.classList.add('card');
             card.innerHTML = `
-      <a href="jeu.html?id=${id}"><img src="assets/image/${index.Image}" alt="Image de jeu"></a> 
-      `
-       
-
-            infos.appendChild(card)
+                <a href="jeu.html?id=${id}">
+                    <img src="assets/image/${game.Image}" alt="Image de jeu">
+                </a>
+            `;
+            infos.appendChild(card);
             id++;
         }
-    })
+    });
+
+
--- assets/JS/jeu.js
+++ assets/JS/jeu.js
@@ -1,38 +1,57 @@
 const gameDetailsContainer = document.getElementById('game-details');
-
-// Récupérer l'ID du jeu à partir de l'URL
 const urlParams = new URLSearchParams(window.location.search);
 const gameId = urlParams.get('id');
 
-// Vérifier si l'ID est valide
 if (!gameId) {
-    // Rediriger vers la page d'accueil ou afficher un message d'erreur
     window.location.href = 'index.html';
 } else {
-    // Charger les détails du jeu en fonction de l'ID
-    fetch(`assets/data/games.json`)
+    fetch('assets/data/games.json')
         .then(response => response.json())
         .then(data => {
             const game = data[gameId];
 
             if (game) {
-                // Afficher les détails du jeu
-                const detailsContainer = document.createElement('div');
-                detailsContainer.classList.add('game-details');
-                detailsContainer.innerHTML = `
+                const card = document.createElement('div');
+                card.classList.add('card');
+
+                const imageDiv = document.createElement('div');
+                imageDiv.classList.add('image-container');
+                imageDiv.innerHTML = `<img src="assets/image/${game.Image}" alt="Image du jeu">`;
+
+                const detailsDiv = document.createElement('div');
+                detailsDiv.classList.add('details-container');
+                detailsDiv.innerHTML = `
                     <h2>${game.Nom}</h2>
-                    <img src="assets/image/${game.Image}" alt="Image du jeu">
                     <p>${game.Description}</p>
-                    <p>Prix: ${game.Prix}</p>
-                    <p>Studio: ${game.Studio}</p>
-                    <p>Plateforme: ${game.Plateforme}</p>
+                    <p class="price">Prix: ${game.Prix}</p>
+                    <p class="studio">Studio: ${game.Studio}</p>
+                    <p class="platform">Plateforme: ${game.Plateforme}</p>
+                    <button class="add-to-cart-btn">Ajouter au panier</button>
+                    <button class="view-reviews-btn">Voir les avis</button>
+                    <div class="reviews-container"></div>
                 `;
 
-                gameDetailsContainer.appendChild(detailsContainer);
+                card.appendChild(imageDiv);
+                card.appendChild(detailsDiv);
+
+                gameDetailsContainer.appendChild(card);
             } else {
-                // Rediriger vers la page d'accueil ou afficher un message d'erreur
                 window.location.href = 'index.html';
             }
-        })
-
+        });
 }
+
+
+const viewReviewsBtn = detailsDiv.querySelector('.view-reviews-btn');
+const reviewsContainer = detailsDiv.querySelector('.reviews-container');
+
+viewReviewsBtn.addEventListener('click', () => {
+    if (game.Avis && game.Avis.length > 0) {
+        const reviewsHTML = game.Avis.map(review => `<p>${review}</p>`).join('');
+        reviewsContainer.innerHTML = reviewsHTML;
+        reviewsContainer.style.display = 'block';
+    } else {
+        reviewsContainer.innerHTML = '<p>Aucun avis disponible.</p>';
+        reviewsContainer.style.display = 'block';
+    }
+});
